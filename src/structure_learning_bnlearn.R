9f9f91ee9c4adf15f49ef2fca442982de920b1fa
Thorben Jensen <jensen.thorben@gmail.com>
2018-12-09 22:20:19 +0100

    containerized Rstudio

--- src/structure_learning_bnlearn.R
+++ src/structure_learning_bnlearn.R
@@ -50,10 +50,9 @@ df2 <-
   mutate(Embarked = as.factor(Embarked)) %>%
   mutate(SibSp = as.factor(SibSp))
 
-# discretize age
 mastercuts <- 
-  arules::discretize(df2$Age, method = "interval", 
-                     categories = 4, onlycuts = T)
+  arules::discretize(df$Age, method = "interval", 
+                     breaks = 3, onlycuts = T)
 df2$Age <- 
   as.numeric(cut(x = df2$Age, breaks = mastercuts))
 
@@ -67,7 +66,8 @@ df3 <-
   filter(complete.cases(.))
 
 res2 <-
-  hc(df3, blacklist = bl2, score = "aic")
+  df3 %>% 
+  hc(., blacklist = bl2, score = "aic")
 
 plot(res2)
 
@@ -78,33 +78,35 @@ fitted <- bn.fit(x = res2, data = df3)
 # idea: for each feature, measure increased certainty due to evidence, 
 #       weighted by unconditional probability of evidence
 
+entropy <- function(p) {
+  if (p == 0 | p == 1) return(0)
+  e <- -p * log2(p) - (1 - p) * log2(1 - p)
+  return(e)
+}
+
 # query and test entropy reduction for different evidence
-prob <- cpquery(fitted, event = (Survived == 1), evidence = TRUE) 
-certainty_baseline <- abs(prob - .5) * 2
+prior <- cpquery(fitted, event = (Survived == 1), evidence = TRUE)
+entropy_baseline <- entropy(prior)
 
 # 'gain of certainty' from sex
 if_male <- cpquery(fitted, event = (Survived == 1), evidence = (Sex == 'male'))
 if_female <- cpquery(fitted, event = (Survived == 1), evidence = (Sex == 'female'))
-prob_male <- cpquery(fitted, event = (Sex == 'male'), evidence = TRUE)
-prob_female <- cpquery(fitted, event = (Sex == 'female'), evidence = TRUE)
+p_male <- cpquery(fitted, event = (Sex == 'male'), evidence = TRUE)
+p_female <- cpquery(fitted, event = (Sex == 'female'), evidence = TRUE)
 
-certainty_sex <- 
-  (prob_male * abs(if_male - .5) + prob_female * abs(if_female - .5)) * 2
+entropy_sex <-
+  (p_male * entropy(if_male)) + (p_female * entropy(if_female))
 
 # 'certainty' with evidence Pclass
-prob_1 <- cpquery(fitted, event = (Pclass == '1'), evidence = TRUE)
+p_1 <- cpquery(fitted, event = (Pclass == '1'), evidence = TRUE)
 if_1 <- cpquery(fitted, event = (Survived == 1), evidence = (Pclass == '1'))
-prob_2 <- cpquery(fitted, event = (Pclass == '2'), evidence = TRUE)
+p_2 <- cpquery(fitted, event = (Pclass == '2'), evidence = TRUE)
 if_2 <- cpquery(fitted, event = (Survived == 1), evidence = (Pclass == '2'))
-prob_3 <- cpquery(fitted, event = (Pclass == '3'), evidence = TRUE)
+p_3 <- cpquery(fitted, event = (Pclass == '3'), evidence = TRUE)
 if_3 <- cpquery(fitted, event = (Survived == 1), evidence = (Pclass == '3'))
 
-certainty_class <-
-  (prob_1 * abs(if_1 - .5) + prob_2 * abs(if_2 - .5) + prob_3 * abs(if_3 - .5)) * 2
+entropy_sex <-
+  p_1 * entropy(if_1) + p_2 * entropy(if_2) + p_2 * entropy(if_2)
 
-
-# check if this 'information gain' measurement makes sense. if yes, scale up.
+# generalize this 'information gain' approach
 # TODO
-
-
-
